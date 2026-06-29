a8d4e1e2513231a412b38f3044f73876cf024fec
mbulganbat <m.bulganbat@gmail.com>
2026-06-29 10:05:31 +0800

    search heseg aldaag zassan

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
 import Navigation from "@/components/Navigation";
@@ -9,11 +9,9 @@ import MovieCard from "@/components/MovieCard";
 import { Footer } from "@/components/Footer";
 import { tmdbHeaders } from "@/lib/tmdb";
 
-const SearchPage = () => {
+const SearchContent = () => {
   const searchParams = useSearchParams();
-
   const query = searchParams.get("search");
-
   const [movies, setMovies] = useState<any[]>([]);
 
   useEffect(() => {
@@ -22,9 +20,7 @@ const SearchPage = () => {
     axios
       .get(
         `https://api.themoviedb.org/3/search/movie?query=${query}&language=en-US&page=1`,
-        {
-          headers: tmdbHeaders,
-        },
+        { headers: tmdbHeaders },
       )
       .then((response) => {
         setMovies(response.data.results);
@@ -35,21 +31,30 @@ const SearchPage = () => {
   }, [query]);
 
   return (
-    <div>
-      <Navigation />
-
-      <div className="flex justify-center">
-        <div className="w-[1280px] px-10 py-10">
-          <h1 className="text-2xl font-bold mb-6">Search Results : {query}</h1>
-
-          <div className="flex flex-wrap gap-5">
-            {movies.map((movie) => (
-              <MovieCard key={movie.id} movie={movie} />
-            ))}
-          </div>
+    <div className="flex justify-center">
+      <div className="w-[1280px] px-10 py-10">
+        <h1 className="text-2xl font-bold mb-6">Search Results : {query}</h1>
+        <div className="flex flex-wrap gap-5">
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
         </div>
       </div>
+    </div>
+  );
+};
 
+const SearchPage = () => {
+  return (
+    <div>
+      <Navigation />
+      <Suspense
+        fallback={
+          <div className="flex justify-center py-20 text-white">Loading...</div>
+        }
+      >
+        <SearchContent />
+      </Suspense>
       <Footer />
     </div>
   );
